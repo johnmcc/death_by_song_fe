5e6f8967593d4898e547d52e74aacad9bc5f9f2f
John McCollum <johnvmccollum@gmail.com>
2019-03-12 16:18:39 +0000

    Adds Title filter, tidy up filter logic

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,31 +3,33 @@ import SongCard from './SongCard'
 import _ from 'lodash'
 
 const SongList = ({songs}) => {
-  let [chosenMethod, setChosenMethod] = useState("")
+  let [method, setMethod] = useState("")
   let [artist, setArtist] = useState("")
+  let [title, setTitle] = useState("")
 
   const songElements = songs
-                      .filter(song => !chosenMethod || song.murderMethods.includes(chosenMethod))
+                      .filter(song => !method || song.murderMethods.includes(method))
                       .filter(song => !artist || song.artist.includes(artist))
+                      .filter(song => !title || song.title.includes(title))
                       .map(song => <SongCard key={song._id["$oid"]} song={song} />)
 
-  const murderOptionMethods = _.chain(songs.map((song) => song.murderMethods))
-                              .flatten()
-                              .uniq()
-                              .map(method =>
-                                <option key={method} value={method}>{method}</option>
-                              )
-                              .value()
+  const murderOptions = _.chain(songs.map((song) => song.murderMethods))
+                      .flatten()
+                      .uniq()
+                      .map(method =>
+                        <option key={method} value={method}>{method}</option>
+                      )
+                      .value()
 
   return (
     <div id="songList">
       <form>
-        <select onChange={(e) => setChosenMethod(e.target.value)}>
+        <select onChange={(e) => setMethod(e.target.value)}>
           <option value="" key="select">Select...</option>
-          {murderOptionMethods}
+          {murderOptions}
         </select>
-
-        <input type="text" onChange={(e) => setArtist(e.target.value) } />
+        <input placeholder="Title" type="text" onChange={(e) => setTitle(e.target.value) } />
+        <input placeholder="Artist" type="text" onChange={(e) => setArtist(e.target.value) } />
       </form>
       {songElements}
     </div>
